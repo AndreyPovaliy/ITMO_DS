007d7d5c58f4e5e57d04a8b390f580ace68c16a8
AndreyPovaliy <povandrey16@yandex.ru>
2024-12-17 14:08:54 +0300

    Add shop shelf

--- 01_programming_basics/program_step.py
+++ 01_programming_basics/program_step.py
@@ -1,58 +1,53 @@
-from random import randint
-import time
+'''
+Настоящая реализация задания с исследованием полки магазина по следующей логике:
+1) Вначале вносят имена об исследуемых полок и описательную часть
+2) В данной программе иметируется продажа за день, а именно случайное количество товара и наименование из
+заданного списка.
+3) Каждый товар имеет заданное значение цены и для каждой полки определяется последовательность покупок и выручка
+4) Разница между полками будет определять непараметрическим критерием, так как будем считать все распределения ненормальными
+'''
 
-def who_champion(a, b):
-  if a > b:
-    return ('Выиграл {0}, число побед: {1}'.format(gamer1, a))
-  elif a < b:
-    return ('Выиграл {0}, число побед: {1}'.format(gamer2, b))
+
+from random import randint,choices
+import scipy.stats as sps
+
+# минимальная реализация описательночасти  
+def input_shelf_data():
+  shelf1 = input('Ввдиете обозначение полки №1 ')
+  shelf2 = input('Ввдиете обозначение полки №2 ')
+  return (shelf1,shelf2)
+
+
+# моделирование количества покупок
+n1 = randint(10, 60)
+n2 = randint(10, 60)
+
+# создание необходимых списков и словарей
+goods = ["Йогрурт Персик","Молоко 2.5%", "Молоко 3.5%","Творог","Детский творожок", "Ряженка","Сметана","Сливки 22%"]
+
+price = [44,54,57,45,32,52,48,31]
+sales1 = []
+sales2 = []
+
+for i in range (30):
+  s1 = random.choices(goods,  k=n1)
+  s2 = random.choices(goods,  k=n2)
+  p1 = random.choices(price,  k=n1)
+  p2 = random.choices(price,  k=n2)
+  sales1 = sales1 + p1
+  sales2 = sales2 + p2
+
+
+mann_pvalue = sps.mannwhitneyu(sales1, sales2, alternative='two-sided').pvalue
+is_sighnificant = mann_pvalue < 0.05
+       
+if(is_sighnificant):
+  print("Разница между полками есть")
+  if(sales1>sales2):
+    print(f"В пользу полки {shelf1}")
   else:
-    return 'Ничья'
+    print(f"В пользу полки {shelf2}")
+else:
+  print("Разницы между полками нет")
   
-  
-def Input_gamer():
-  ig1 = input('Введите имя 1-го играющего ')
-  ig2 = input('Введите имя 2-го играющего ')
-  return (ig1, ig2) # возвращается кортеж из двух переменных
 
-
-
-#Ввод имен играющих
-gamer1, gamer2 = Input_gamer()
-
-k1 = k2 = 0 # количество побед каждого игрока
-rezult_g1 = []
-rezult_g2 = []
-
-for i in range(5):
-  #Моделирование бросания кубика первым играющим
-  print('Кубик бросает', gamer1)
-  time.sleep(2)
-  n1 = randint(1, 6)
-  rezult_g1.append(n1)
-  print('Выпало:', n1)
-
-  #Моделирование бросания кубика вторым играющим
-  print('Кубик бросает', gamer2)
-  time.sleep(2)
-  n2 = randint(1, 6)
-  rezult_g2.append(n2)
-  print('Выпало:', n2)
-  #Определение результата (3 возможных варианта)
-  
-  if n1 > n2:
-    print('В раунде выиграл', gamer1)
-    k1 += 1
-  elif n1 < n2:
-    print('В раунде выиграл', gamer2)
-    k2 += 1
-  else:
-    print('Ничья')
-    
-# определение итогового победителя
-fin = who_champion(k1,k2)
-print("Итог игры: ", fin)
-  
-  
-print("Результаты первого игрока: ", rezult_g1)
-print("Результаты второго игрока: ", rezult_g2)
