98945773ed737ea48c91ee2f79cc2d366f13b6bd
AndreyPovaliy <povandrey16@yandex.ru>
2025-04-16 13:35:49 +0300

    fix bash file

--- 06_data_visualisation/run_polution_dash.bash
+++ 06_data_visualisation/run_polution_dash.bash
@@ -1,2 +1,4 @@
-pip install -r ./06_data_visualisation/python/polution_dash/requirements.txt
+python3 -m venv ./06_data_visualisation/python/polution_dash/my_env
+source ./06_data_visualisation/python/polution_dash/my_env/bin/activate
+pip3 install -r ./06_data_visualisation/python/polution_dash/requirements.txt
 python3 ./06_data_visualisation/python/polution_dash/app.py
