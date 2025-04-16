ab375f498a89b428b911abeca0f5dae733b904a1
AndreyPovaliy <povandrey16@yandex.ru>
2025-04-16 13:13:11 +0300

    End 1st task in 6th course

---
+++ 06_data_visualisation/06_data_visualisation.md
@@ -0,0 +1,5 @@
+**Практические задания**
+
+[Задание No1. «Дашборд состояния воздуха в городе»](https://github.com/AndreyPovaliy/ITMO_DS/blob/main/06_data_visualisation/python/polution_dash)
+
+[Задание No2. Deploy «Дашборд состояния воздуха в городе» (в процессе)]()
---
+++ 06_data_visualisation/python/weather_dash/layouts.py
@@ -0,0 +1,27 @@
+import dash_bootstrap_components as dbc
+from dash import dcc
+
+def create_layout():
+    return dbc.Container([
+        dbc.NavbarSimple(
+            brand="🌦 Прогноз  погоды 🌦",
+            brand_href="#",
+            color="primary",
+            dark=True,
+            className="mb-4 flex justify-content-between"
+        ),
+        dbc.Row([
+            dbc.Col([dbc.Input(id = "city-input", type = "text", placeholder = "Введите город")], width = 6, xs = 12, md = 6),
+            dbc.Col(dbc.Card(id='weather-output', body=True), width=6, xs=12, md=6), 
+            ], className="mb-3"), 
+            
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='temp-graph'), width=6, xs=12, md=6),
+            dbc.Col(dcc.Graph(id='ap-graph'), width=6, xs=12, md=6),
+        ], className="mb-3"),
+
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='wind-dir-graph'), width=12, xs=12, md=12),
+        ], className="mb-3")],
+            
+            fluid = True)
